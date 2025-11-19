e4da237c002b9ee8db88548e119d9fbb9445e0ca
이형주 <jujumom630@gmail.com>
2025-11-19 23:40:15 +0900

    TMP 임포트 & 간단 UI

--- Assets/Scripts/Controll.cs
+++ Assets/Scripts/Controll.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class Controll : MonoBehaviour
+public class Controll : MonoBehaviourPunCallbacks
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -4,12 +4,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    public InputField NicknameInput;
-    public GameObject SelectButton;
-    public GameObject Select;
+    public TMP_InputField NicknameInput;
+    public TMP_InputField RoomNumberInput;
+    public GameObject ConnectButton;
     void Awake()
     {
         PhotonNetwork.SendRate = 60;
@@ -20,12 +21,34 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
+        string RoomNum = RoomNumberInput.text;
+        if(RoomNum.Length != 5)
+        {
+            Error_five();
+            return;
+        }
         PhotonNetwork.LocalPlayer.NickName = NicknameInput.text;
-        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 2 }, null);
+        PhotonNetwork.JoinOrCreateRoom(RoomNum, new RoomOptions { MaxPlayers = 2 }, null);
     }
 
     public override void OnJoinedRoom()
     {
-        
+        // 씬 넘어가기
+        // 동기화 뭐시기 로딩 씬
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        // 연결 끊겼을 시
+    }
+
+    void Update()
+    {
+        // PhotonNetwork.Disconnect();
+    }
+
+    public void Error_five()
+    {
+        // 다섯자 안되거나 넘으면 이거
     }
 }
