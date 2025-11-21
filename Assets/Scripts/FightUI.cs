712ed9c57d2963ed67ac95b2d07efe0d8b84ff9e
이형주 <jujumom630@gmail.com>
2025-11-21 10:32:17 +0900

    남은 시간 표시

--- Assets/Scripts/FightUI.cs
+++ Assets/Scripts/FightUI.cs
@@ -10,9 +10,11 @@ public class FightUI : MonoBehaviourPunCallbacks, IPunObservable
 {
     public Image HP_1;
     public Image HP_2;
+    public TMP_Text Seconds;
+    public float TimeMax;
     void Awake()
     {
-        
+        Seconds.text = TimeMax.ToString("0");
     }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
@@ -22,11 +24,14 @@ public class FightUI : MonoBehaviourPunCallbacks, IPunObservable
     // Update is called once per frame
     void Update()
     {
-        
+        TimeMax -= Time.deltaTime;
+        Seconds.text = TimeMax.ToString("0");
     }
 
-    [PunRPC] // 일시적으로 모든 부분에서 사용
-    void checkUI(float CurHP1, float MaxHP1, float CurHP2, float MaxHP2) // 사용 예 : photonView.RPC("checkUI", RpcTarget.All, 60, 100);
+    [PunRPC] 
+    // 일시적으로 모든 컴에서 사용
+    // 사용 예 : photonView.RPC("checkUI", RpcTarget.All, 60, 100);
+    void checkUI(float CurHP1, float MaxHP1, float CurHP2, float MaxHP2) 
     {
         HP_1.fillAmount = CurHP1 / MaxHP1;
         HP_2.fillAmount = CurHP2 / MaxHP2;
