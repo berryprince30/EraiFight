84f2b60694267911b9d769c9ef8f0d968fadc93c
이형주 <jujumom630@gmail.com>
2025-11-26 00:05:20 +0900

    문구 업데이트

--- Assets/Scripts/Start/SelectManage.cs
+++ Assets/Scripts/Start/SelectManage.cs
@@ -16,7 +16,10 @@ public class SelectManage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseCharInfo();
+        }
     }
 
     public void Selct(int a)
