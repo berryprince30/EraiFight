8204710ab77b7ecfa526aeb64b8392108b91d438
이형주 <jujumom630@gmail.com>
2025-11-26 15:24:03 +0900

    우와 리소시즈 폴더를 따로 만들어야 하는구나 제기랄
    포톤 신기하네...아니 병신같네
    아니 빌드하니까 안돼는데 쌰갈
    앙 기모찌 그 문제가 아니였노 병신 포톤뷰

--- Assets/Scripts/Fight/FightManage.cs
+++ Assets/Scripts/Fight/FightManage.cs
@@ -10,10 +10,12 @@ using TMPro;
 public class FightManage : MonoBehaviour
 {
     public GameObject[] Char_Pres;
+    int MasterIndex;
+    int ClientIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CharSpwan();
     }
 
     // Update is called once per frame
@@ -24,6 +26,15 @@ public class FightManage : MonoBehaviour
 
     public void CharSpwan()
     {
-        
+        Vector3 Mvec = new Vector3(-5, 0, 0);
+        Vector3 Cvec = new Vector3(5, 0, 0);
+        if(PhotonNetwork.IsMasterClient) // -5 0 0
+        {
+            PhotonNetwork.Instantiate(Char_Pres[0].name, Mvec, Quaternion.identity);
+        }
+        else // 5 0 0
+        {
+            PhotonNetwork.Instantiate(Char_Pres[1].name, Cvec, Quaternion.identity);
+        }
     } 
 }
